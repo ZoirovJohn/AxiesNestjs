11ae299a3d61fecf125b273f8db22abd9730eaf8
Thomas <zoirovjohn@gmail.com>
2025-03-08 22:56:51 +0900

    fix: dotenv MONGO_PROD is fixed

--- apps/nestar-api/src/train.ts
+++ apps/nestar-api/src/train.ts
@@ -18,3 +18,23 @@ function printNumbers(arr: any[]) {
 }
 
 printNumbers([1, [1, 2, [4]]]);
+//****************************************************** */
+/**TASK-ZK:
+
+Shunday function yozing, u har soniyada bir marta consolega 1 dan 5 gacha bolgan raqamlarni chop etsin va 5 soniyadan keyin ishini toxtatsin.
+MASALAN: printNumbers()
+
+
+function printNumbers(arr: any[]) {
+	const interval = setInterval(() => {
+		for (let i = 1; i <= 5; i++) {
+			console.log(i);
+		}
+	}, 1000);
+	setTimeout(() => {
+		clearInterval(interval);
+	}, 5000);
+}
+
+printNumbers([1, [1, 2, [4]]]);
+*/
