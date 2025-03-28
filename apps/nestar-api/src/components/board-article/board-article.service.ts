7a17595d1c1c141984b9ef120a304afc4a3624ec
Thomas <zoirovjohn@gmail.com>
2025-03-29 01:16:06 +0900

    feat: develop admin related boardArticle GraphQL APIs

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -12,6 +12,7 @@ import { InjectModel } from "@nestjs/mongoose";
 import { MemberService } from "../member/member.service";
 import { ViewService } from "../view/view.service";
 import {
+  AllBoardArticlesInquiry,
   BoardArticleInput,
   BoardArticlesInquiry,
 } from "../../libs/dto/board-article/board-article.input";
@@ -163,6 +164,85 @@ export class BoardArticleService {
       throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
     return result[0];
+  }
+
+  public async getAllBoardArticlesByAdmin(
+    input: AllBoardArticlesInquiry
+  ): Promise<BoardArticles> {
+    const { articleStatus, articleCategory } = input.search;
+    const match: T = {};
+    const sort: T = {
+      [input?.sort ?? "createdAt"]: input?.direction ?? Direction.DESC,
+    };
+
+    if (articleStatus) match.articleStatus = articleStatus;
+    if (articleCategory) match.articleCategory = articleCategory;
+
+    const result = await this.boardArticleModel
+      .aggregate([
+        { $match: match },
+        { $sort: sort },
+        {
+          $facet: {
+            list: [
+              { $skip: (input.page - 1) * input.limit },
+              { $limit: input.limit },
+              // meLiked
+              lookupMember,
+              { $unwind: "$memberData" },
+            ],
+            metaCounter: [{ $count: "total" }],
+          },
+        },
+      ])
+      .exec();
+    if (!result.length)
+      throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+    return result[0];
+  }
+
+  public async updateBoardArticleByAdmin(
+    input: BoardArticleUpdate
+  ): Promise<BoardArticle> {
+    const { _id, articleStatus } = input;
+
+    const result = await this.boardArticleModel
+      .findByIdAndUpdate(
+        {
+          _id: _id,
+          articleStatus: BoardArticleStatus.ACTIVE,
+        },
+        input,
+        { new: true }
+      )
+      .exec();
+    if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+    if (articleStatus === BoardArticleStatus.DELETE) {
+      await this.memberService.memberStatsEditor({
+        _id: result.memberId,
+        targetKey: "memberArticles",
+        modifier: -1,
+      });
+    }
+
+    return result;
+  }
+
+  public async removeBoardArticleByAdmin(
+    articleId: ObjectId
+  ): Promise<BoardArticle> {
+    const search: T = {
+      _id: articleId,
+      articleStatus: BoardArticleStatus.DELETE,
+    };
+    const result = await this.boardArticleModel
+      .findByIdAndDelete(search)
+      .exec();
+    if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+    return result;
   }
 
   public async boardArticleStatsEditor(
