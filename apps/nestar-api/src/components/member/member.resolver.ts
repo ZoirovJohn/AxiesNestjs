7407361f8d3c49c565ea00f90dd30557885305e8
Thomas <zoirovjohn@gmail.com>
2025-04-01 06:51:59 +0900

    feat: develop likeTargetMember GraphQL api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -94,6 +94,16 @@ export class MemberResolver {
     return await this.memberService.getAgents(memberId, input);
   }
 
+  @UseGuards(AuthGuard)
+  @Mutation(() => Member)
+  public async likeTargetMember(
+    @Args("memberId") input: string,
+    @AuthMember("_id") memberId: ObjectId
+  ): Promise<Member> {
+    const likeRefId = shapeIntoMongoObjectId(input);
+    return await this.memberService.likeTargetMember(memberId, likeRefId);
+  }
+
   /** ADMIN **/
 
   @Roles(MemberType.ADMIN)
