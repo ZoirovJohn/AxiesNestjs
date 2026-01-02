9ce8d794d59334081aadb145c86b597c01e86329
Tokhirjon Zoirov <zoirovtokhirjon@gmail.com>
2026-01-02 15:51:33 +0900

    fix: made some changes

--- apps/axies-api/src/app.module.ts
+++ apps/axies-api/src/app.module.ts
@@ -3,52 +3,27 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { ApolloDriver } from "@nestjs/apollo";
 import { AppResolver } from "./app.resolver";
 import { ComponentsModule } from "./components/components.module";
 import { DatabaseModule } from "./database/database.module";
 import { T } from "./libs/types/common";
-import { SocketModule } from "./socket/socket.module";
+import { SocketModule } from './socket/socket.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRoot({
       driver: ApolloDriver,
       playground: true,
+      uploads: false,
       autoSchemaFile: true,
-      path: "/graphql",
-      subscriptions: {
-        "graphql-ws": {
-          path: "/graphql",
-          onConnect: (context) => {
-            const token =
-              context.connectionParams?.authorization ||
-              context.connectionParams?.Authorization ||
-              context.connectionParams?.token;
-
-            if (!token) {
-              throw new Error("Missing auth token");
-            }
-
-            return { token };
-          },
-        },
-      },
-
-      context: ({ req, connection }) => {
-        if (connection) {
-          return connection.context;
-        }
-        return { req };
-      },
-
       formatError: (error: T) => {
         const graphQLFormattedError = {
-          code: error?.extensions?.code,
+          code: error?.extensions.code,
           message:
             error?.extensions?.exception?.response?.message ||
-            error?.extensions?.response?.message ||
+            error?.extensions.response?.message ||
             error?.message,
         };
         console.log("GRAPHQL GLOBAL ERR:", graphQLFormattedError);
